4b795cd64270a22ae398e2f11f08a54cefc930a7
Mélanie Proix <jnmelanie@gmail.com>
2020-11-25 22:45:51 +0100

    all about page in rwd

--- about.php
+++ about.php
@@ -6,11 +6,88 @@
             <h1>Profil général : Intégratrice web</h1>
         </div>
         <div id="left_cv_part">
-            <div id="dates">2020</div>
-            <div id="text_content">Compétences</div>
+            <div id="dates">
+                <p>2020</p>
+                <p>2017</p>
+                <p>11-12-2018</p>
+                <p>2014-2020</p>
+                <p>2006-2009</p>
+                <p>2017-2020</p>
+                <p>2019</p>
+                <p>2014</p>
+                <p>2014</p>
+                <p>2011</p>
+            </div>
+            <div id="text_content">
+                <h2>Compétences techniques</h2>
+                <ul>
+                    <li>référencement web (par exemple : www.esther-aline-laiser.com)</li>
+                    <li>conception et construction d’une interface utilisateur</li>
+                    <li>articulation algorithme/interface</li>
+                    <li>sites en responsive web design</li>
+                </ul>
+                <h2>Autres compétences</h2>
+                <ul>
+                    <li>les projets difficiles me stimulent</li>
+                    <li>j’aime communiquer et j’entre facilement en relation</li>
+                    <li>mon travail est soigné et organisé clairement</li>
+                </ul>
+                <h2>Expérience professionnelle</h2>
+                <ul>
+                    <li class="dated">rédaction de sites Web natifs pour professionnels et associations en freelance : HTML/CSS/JavaScript/PHP/JSON</li>
+                    <li>projets d’applications JavaScript (calendar)</li>
+                    <li class="dated">projet Python pour professeurs de collège (formation automatisée de groupes d’élèves)</li>
+                </ul>
+                <p class="dated"><strong>Stage en entreprise CASC4DE</strong> (http://www.casc4de.eu/) Interface graphique Tilt (logiciel scientifique)</p>
+                <p class="dated"><strong>Professeure de français en collège, Éducation Nationale</strong> (Strasbourg, Haguenau, Colmar, Matzenheim)</p>
+                <p class="dated"><strong>Responsable scout :</strong></p>
+                <ul>
+                    <li>encadrement d’une équipe de jeunes adultes dans la mise en place de projets et l’organisation de camps</li>
+                    <li>encadrement de jeunes de 8 à 14 ans</li>
+                </ul>
+                <h2>Formation académique</h2>
+                <p class="dated"><strong>OpenClassrooms : </strong>suivi et validation de cours</p>
+                <p class="dated"><strong>Cursus Python premier niveau, ESBS Illkirch</strong></p>
+                <p class="dated"><strong>CAFEP de Lettres Modernes</strong></p>
+                <p class="dated"><strong>Master Lettres Modernes, Strasbourg</strong></p>
+                <p class="dated"><strong>Licence LLCE Espagnol, Lyon 2</strong></p>
+
+                <h2>Centres d'intérêt</h2>
+                <p><strong>Dessin à la tablette : </strong>Krita, Gimp, Photoshop, PENUP, Inkscape, Blender</p>
+                <p><strong>Musique : </strong>violon, flûte, guitare et chant</p>
+                <p><strong>Bricolage et menuiserie</strong></p>
+            </div>
         </div>
-        <div id="my_infos">Mélanie Proix</div>
-        <div id="interests">Bricolage</div>
+        <div id="my_infos">
+            <h3>Mélanie Proix</h3>
+            <h2>Informations personnelles</h2>
+            <p><strong>Téléphone</strong></p>
+            <p>+33 6 95 62 24 75</p>
+            <p><strong>Courriel</strong></p>
+            <p><a href="mailto:melanie@proix.eu">melanie@proix.eu</a></p>
+            <p><a href="https://github.com/MProix">https://github.com/MProix</a></p>
+            <p><strong>Site web</strong></p>
+            <p><a href="https://www.proix.eu/">https://www.proix.eu/</a></p>
+            <p>Titulaire du permis B</p>
+            <h2>Langages informatiques</h2>
+            <p><strong>HTML5/CSS3 +++</strong></p>
+            <p><strong>JavaScript ++</strong></p>
+            <p><strong>PHP, Python +</strong></p>
+            <p><strong>Ruby, mySQL</strong></p>
+            <h2>Frameworks</h2>
+            <p><strong>jQuery, Bootstrap +++</strong></p>
+            <p><strong>Jupyter ++</strong></p>
+            <p><strong>Flask ++</strong></p>
+            <h2>OS</h2>
+            <p><strong>Linux</strong></p>
+            <p><strong>Windows</strong></p>
+            <p><strong>Android</strong></p>
+            <h2>Langues</h2>
+            <p><strong>Espagnol - courant</strong></p>
+            <p><strong>Anglais - bon niveau</strong></p>
+            <p>B2 universitaire</p>
+            <p>Pratique des deux langues en séjours internationaux</p>
+        </div>
     </div>
     <div id="right_main_content"></div>
 </div>
--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,21 @@
 
 $("#main_content").css("height", parseInt(getComputedStyle(document.getElementById("main_content")).width)*Math.sqrt(2));
+correspondHeights();
 
+function correspondHeights(){
+    let num = 1;
+    for (let elt of ($("#dates p"))){
+        elt.style.top = (($(".dated")[num-1].offsetTop));
+        num ++;
+        
+    };
+};
 document.getElementById("body").onresize = function(e){
     var ownWidth = window.innerWidth;
     var ownHeight = window.innerHeight;
     $("#main_content").css("height", parseInt(getComputedStyle(document.getElementById("main_content")).width)*Math.sqrt(2));
-
-    console.log("e -> ",e.target.innerWidth);
+    correspondHeights();
+    // console.log("e -> ",e.target.innerWidth);
 };
+
+
