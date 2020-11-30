02c450cc80abe9c966be97946b99212943242119
Mélanie Proix <jnmelanie@gmail.com>
2020-11-30 23:27:31 +0100

    js + css homepage

--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,26 @@
 //-----------------------------Animation avatar on home page----------------------------------//
-//animation div height and resizable height
-var jsAnimation = document.createElement("div");
-jsAnimation.id = "jsAnimation";
-$(jsAnimation).insertAfter("#header");
+//animation div height and resizable height----------------------------------
 $("#jsAnimation").css("height",window.innerHeight - $("header").height());
 
 document.getElementById("body").onresize = function(e){
     $("#jsAnimation").css("height",window.innerHeight - $("header").height());
+    if (window.matchMedia("(min-width: 992px)").matches) {
+        console.log("resizing");
+        $("#web").css("width", 100-(((100*($(".facePlaced")[0].width))/window.innerWidth)+5)+"vw");
+        $("#drawings").css("width", 100-(((100*($(".facePlaced")[0].width))/window.innerWidth)+5)+"vw");
+        $("#others").css("width", 100-(((100*($(".facePlaced")[0].width))/window.innerWidth)+5)+"vw");
+    } else {
+        $("#web").css("width", 100-(((100*($(".facePlaced")[0].width))/window.innerWidth)+5)+"vw");
+    };
 };
-//div for avatar image
+
+//div for avatar image-------------------------------------------------------
 var avatarDiv = document.createElement("div");
 avatarDiv.id = "avatarDiv";
 avatarDiv.style.height = toVh(vhToPx(100)-($("#header").height()))-2+"vh";
 jsAnimation.appendChild(avatarDiv);
 
-//animation div imgs -> adding on page
+//animation div imgs -> adding on page---------------------------------------
 function addFacePart(name, identifier,source, parent="avatarDiv",avClass="avatarPart"){
     var name = document.createElement("img");
     name.id = identifier;
@@ -34,6 +40,22 @@ $(document).ready(function(){
         $("#"+elt.id).addClass("facePartPlaced");
         $("#"+elt.id).addClass(elt.id+"Placed");
     };
+    if (window.matchMedia("(min-width: 992px)").matches) {
+        $("#web").css("width", 100-(toVw($(".facePlaced")[0].width)+5)+"vw");
+        $("#drawings").css("width", 100-(toVw($(".facePlaced")[0].width)+5)+"vw");
+        $("#others").css("width", 100-(toVw($(".facePlaced")[0].width)+5)+"vw");
+    } else {
+        $("#webText>a").html("<p>W</p><p>E</p><p>B</p>");
+        $("#web").css("width", 100-(toVw($(".facePlaced")[0].width)+5)+"vw");
+    };
+    for (let elt of $(".otherHomePart")){
+        $("#"+elt.id).addClass(elt.id+"Placed");
+    };
 });
 
-//--------------------------Other parts of home page -----------------------------------//
+
+
+
+
+
+
